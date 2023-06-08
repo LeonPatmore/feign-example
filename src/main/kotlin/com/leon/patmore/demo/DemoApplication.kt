e237fc6ee654356773211233272971f6fadb8f15
Leon Patmore <leon.patmore@bath.edu>
2023-06-08 23:14:22 +0100

    first commit

---
+++ src/main/kotlin/com/leon/patmore/demo/DemoApplication.kt
@@ -0,0 +1,11 @@
+package com.leon.patmore.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
